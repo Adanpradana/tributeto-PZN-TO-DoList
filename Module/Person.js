85e5017915d2aa3e6b3473acb8a84fb0f51e391d
Adanpradana <adanizkahariz@gmail.com>
2022-07-14 10:03:57 +0700

    OOP done

--- Module/Person.js
+++ Module/Person.js
@@ -1,10 +1,53 @@
 class Person {
   constructor() {
     this._todoBody = document.getElementById("todoBody");
+    this._todoList = [];
   }
   get clearing() {
     while (this._todoBody.firstChild) {
       this._todoBody.removeChild(this._todoBody.firstChild);
     }
   }
+
+  get displaying() {
+    for (let i = 0; i < todoList.length; i++) {
+      const todo = todoList[i];
+      const searchText = document.getElementById("search").value;
+      const goTolowerCase = searchText.toLowerCase();
+      if (todo.toLowerCase().includes(goTolowerCase)) {
+        this.createTodoList(i, todo);
+      }
+    }
+    // person.todoListValue;
+  }
+  createTodoList = (index, todo) => {
+    const tr = document.createElement("tr");
+    const tdButton = document.createElement("td");
+    tr.appendChild(tdButton);
+
+    const tdTodo = document.createElement("td");
+    tdTodo.textContent = todo;
+    tr.appendChild(tdTodo);
+
+    const todoBody = document.getElementById("todoBody");
+    todoBody.appendChild(tr);
+
+    const createButton = document.createElement("input");
+    createButton.type = "button";
+    createButton.value = "Done";
+    createButton.onclick = function (index) {
+      removeList(index);
+    };
+    tdButton.appendChild(createButton);
+  };
+
+get triggerForm(){
+  const todo = document.forms["toDoform"]["todo"];
+  const todoValue = todo.value;
+  todoList.push(todoValue);
+  console.log(todoList);
+  submitForms.reset();
+  displayTodoList();
+}
+
 }
--- script.js
+++ script.js
@@ -5,53 +5,20 @@ const button = document.getElementById("button");
 
 const clearTodoList = () => person.clearing;
 
+const displayTodoList = () => {
+  person.clearing;
+  person.displaying;
+};
+
 function removeList(index) {
   todoList.splice(index, 1);
   displayTodoList();
   console.log(index);
 }
 
-const createTodoList = (index, todo) => {
-  const tr = document.createElement("tr");
-  const tdButton = document.createElement("td");
-  tr.appendChild(tdButton);
-
-  const tdTodo = document.createElement("td");
-  tdTodo.textContent = todo;
-  tr.appendChild(tdTodo);
-
-  const todoBody = document.getElementById("todoBody");
-  todoBody.appendChild(tr);
-
-  const createButton = document.createElement("input");
-  createButton.type = "button";
-  createButton.value = "Done";
-  createButton.onclick = function (index) {
-    removeList(index);
-  };
-  tdButton.appendChild(createButton);
-};
-
-const displayTodoList = () => {
-  clearTodoList();
-  for (let i = 0; i < todoList.length; i++) {
-    const todo = todoList[i];
-    const searchText = document.getElementById("search").value;
-    const goTolowerCase = searchText.toLowerCase();
-    if (todo.toLowerCase().includes(goTolowerCase)) {
-      createTodoList(i, todo); //method
-    }
-  }
-};
-
 submitForms.onsubmit = function (event) {
   event.preventDefault();
-  const todo = document.forms["toDoform"]["todo"];
-  const todoValue = todo.value;
-  todoList.push(todoValue);
-  console.log(todoList);
-  submitForms.reset();
-  displayTodoList();
+  person.triggerForm;
 };
 
 const searchInput = document.getElementById("search");
@@ -67,6 +34,4 @@ searchInput.onkeydown = () => {
 
 displayTodoList();
 
-//search function
 
-function search() {}
